5f1de88aabb5376f6a4c37681197a904adcfaa1c
Quixelation <58915890+Quixelation@users.noreply.github.com>
2021-03-10 19:21:14 +0100

    How To Use

--- src/manager/src/navbar.ts
+++ src/manager/src/navbar.ts
@@ -2,7 +2,12 @@ export default function prependNavBarButtons() {
   document.querySelector(".site-name").innerHTML += `
   <span style="font-size: 0.75rem; display: block;margin-top: -4px; color: grey;">Tools aktiv!</span>
 `;
-  function createNavBarButton(text: string, link: string) {
+  function createNavBarButton(
+    text: string,
+    link: string,
+    margin?: true,
+    blank?: true
+  ) {
     const buttonContainer = document.createElement("li");
     buttonContainer.classList.add("nav-item");
 
@@ -10,11 +15,22 @@ export default function prependNavBarButtons() {
     button.classList.add("btn", "btn-secondary");
     button.innerHTML = text;
     button.href = link;
+    if (blank) button.target = "_blank";
     buttonContainer.append(button);
-
+    if (margin) buttonContainer.style.marginLeft = "8px";
     return buttonContainer;
   }
   const NavBar = document.querySelector(".navbar-nav.d-none.d-md-flex");
+  NavBar.prepend(
+    createNavBarButton(
+      `<img style="height: 20px;" src="${chrome.runtime.getURL(
+        "icons/icon.png"
+      )}" /> Tools`,
+      "https://smjt.robertstuendl.com/how-to-use",
+      true,
+      true
+    )
+  );
   NavBar.prepend(
     createNavBarButton(
       '<i class="icon fa fa-tachometer fa-fw"></i>Dashboard',
