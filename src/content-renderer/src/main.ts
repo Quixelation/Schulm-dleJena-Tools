24e6171bc3013537c38c212181cc5bdb251ca1f5
Quixelation <58915890+Quixelation@users.noreply.github.com>
2021-03-21 18:48:45 +0100

    Update main.ts

--- src/content-renderer/src/main.ts
+++ src/content-renderer/src/main.ts
@@ -5,37 +5,10 @@ const sectionElem = document.createElement("section");
 sectionElem.id = "AsideSectionElem_TodosManager";
 sectionElem.classList.add("block_navigation", "block", "card", "mb-3");
 document.querySelector("#block-region-side-pre").prepend(sectionElem);
-var app;
-if (!location.pathname.includes("/mod/quiz/")) {
-  app = new App({
-    target: document.querySelector("section#AsideSectionElem_TodosManager"),
-    props: {},
-  });
-}
+
+var app = new App({
+  target: document.querySelector("section#AsideSectionElem_TodosManager"),
+  props: {},
+});
+
 export default app;
-
-function deleteTodoItem(key) {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(["todos"], (val) => {
-      var { todos } = val;
-      delete todos[key];
-      chrome.storage.sync.set({ todos }, () => {
-        resolve();
-      });
-    });
-  });
-}
-
-function changeDoneState(key, state) {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(["todos"], (val) => {
-      var { todos } = val;
-      todos[key].done = state;
-      chrome.storage.sync.set({ todos }, () => {
-        resolve();
-      });
-    });
-  });
-}
-
-export { deleteTodoItem, changeDoneState };
